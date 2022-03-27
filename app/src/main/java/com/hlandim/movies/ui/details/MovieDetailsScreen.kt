99c424f80e6b2d60b309368474f6c9280a18142b
hlandim <hlandim.s@gmail.com>
2022-03-27 03:03:41 +0100

    - adding readme imgs.

--- app/src/main/java/com/hlandim/movies/ui/details/MovieDetailsScreen.kt
+++ app/src/main/java/com/hlandim/movies/ui/details/MovieDetailsScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
@@ -107,7 +108,10 @@ fun MovieDetails(movie: Movie) {
                 },
                 // Used by Compose compile when using preview tab
                 previewPlaceholder = R.drawable.ic_launcher_background,
-                modifier = Modifier.semantics {
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(180.dp)
+                    .semantics {
                     contentDescription = "backDropPath"
                 }
             )
